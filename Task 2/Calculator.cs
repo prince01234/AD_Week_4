eb1a309e0d73ce415f399b1928414e1fa55c580e
Prince <princeshrestha2111018@gmail.com>
2025-11-18 21:03:20 +0545

    task 2 completed

---
+++ Task 2/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Calculator
+    {
+        public static void PrintWelcome()
+        {
+            Console.WriteLine("Welcome to the Calculator!");
+        }
+
+        public static int Add(int num1, int num2 )
+        {
+            return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int? num2)
+        {
+            int secondNumber = num2 ?? 1;
+            return num1 * secondNumber;
+        }
+
+        }
+    }
---
+++ Task 2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.PrintWelcome();
+
+            int sum = Calculator.Add(20, 30);
+            Console.WriteLine($"Their sum is: {sum}");
+
+            int product = Calculator.Multiply(10, null);
+            Console.WriteLine($"Their product is: {product}");
+        }
+    }
+}
