4f7c69ac6130449817d705aaf9bd1b8cd1f41f48
Prince <princeshrestha2111018@gmail.com>
2025-11-18 21:53:20 +0545

    Task 4 Completed

---
+++ Task 4/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Player
+    {
+        public string playerName;
+        public int level, health;
+
+        public Player()
+        {
+            Console.WriteLine("Default constructor has been called.");
+        }
+
+        public Player(string playerName, int level , int health )
+        {
+            this.playerName = playerName;
+            this.level = level;
+            this.health = health;
+        }
+    }
+}
---
+++ Task 4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p1 = new Player();
+            Console.WriteLine("Player 1:");
+            Console.WriteLine("Name: " + p1.playerName);
+            Console.WriteLine("Level: " + p1.level);
+            Console.WriteLine("Health: " + p1.health);
+
+            Console.WriteLine();
+
+            // Create object using parameterized constructor
+            Player p2 = new Player("Prince Shrestha", 999, 99999);
+            Console.WriteLine("Player 2:");
+            Console.WriteLine("Name: " + p2.playerName);
+            Console.WriteLine("Level: " + p2.level);
+            Console.WriteLine("Health: " + p2.health);
+        }
+    }
+}
