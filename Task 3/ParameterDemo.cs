ba16d445b752874d9bb9b04ab9e5cd16ea2d2744
Prince <princeshrestha2111018@gmail.com>
2025-11-18 21:25:20 +0545

    Task 3 Completed

---
+++ Task 3/ParameterDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class ParameterDemo
+    {
+        public static int Increase(ref int number)
+        {
+            number += 10;
+            return number;
+        }
+
+        public static string GetFullName(out string fullName)
+        {
+            return fullName = "Prince Shrestha";
+        }
+
+        public static int SumAll(params int[] numbers)
+        {
+            int sum = 0;
+    
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Task 3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for Ref
+            int number = 3;
+            Console.WriteLine($"Before Increase: {number}");
+            int increasedNum = ParameterDemo.Increase(ref number);
+            Console.WriteLine($"After Increase: {increasedNum}");
+
+            //for Out
+            string nameOut = ParameterDemo.GetFullName(out string fullName);
+            Console.WriteLine($"GetFullName: {fullName}");
+
+            //for sumAll
+            int totalSum = ParameterDemo.SumAll(1, 3, 5, 7, 11);
+            Console.WriteLine($"Sum of all numbers: {totalSum}");
+        }
+    }
+}
