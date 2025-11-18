608a7cd5bac6b7d81a2e5bd567f172d635283132
Prince <princeshrestha2111018@gmail.com>
2025-11-18 11:52:46 +0545

    task 1 completed

---
+++ Task 1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            student1.Name = "Prince Shrestha";
+            student1.Age = 21;
+            student1.Email = "princeshrestha@gmail.com";
+
+            Student student2 = new Student();
+            student2.Name = "Stacy Rose";
+            student2.Age = 21;
+            student2.Email = "stacy.rose@email.com";
+
+            PrintStudent(student1);
+            PrintStudent(student2);
+        }
+
+        public static void PrintStudent(Student student)
+        {
+            Console.WriteLine("Name: " + student.Name);
+            Console.WriteLine("Age: " + student.Age);
+            Console.WriteLine("Email: " + student.Email);
+            Console.WriteLine("University: " + Student.University);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Task 1/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Student
+    {
+        public string Name;
+        public int Age;
+        public string Email;
+
+        public static string University = "Itahari International College"; 
+    }
+}
